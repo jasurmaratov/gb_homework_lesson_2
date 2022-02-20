6e22a596098dd4315f1d02148faceab8e5e9cf4a
Jasur <jasur-maratov93@mail.ru>
2022-02-20 21:41:52 +0300

    jasur_maratov_dz_5.py

--- jasur_maratov_dz_5.py
+++ jasur_maratov_dz_5.py
@@ -1,7 +1,11 @@
 input_list = [57.8, 46.51, 97, 76.05, 13.11, 87.93, 27, 97.09, 0.16, 42,
               96.64, 34.17, 97.45, 40.62, 84.94, 7, 52.23, 93.74, 89, 3.93]
 
-store_id = id(input_list)
-print(store_id)
+for i, num in enumerate(input_list):
 
-for i,
+    fix_price = str(f"{float(num):.2f}").split(".")
+
+    if i == len(input_list) - 1:
+        end_word = "\n"
+
+    print(f"{fix_price[0]} руб {fix_price[1]} коп")
